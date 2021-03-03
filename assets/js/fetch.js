86d2c4715def92be3a5dc2866274d82d8e61d23f
Mekides <se.mekides.bishaw@gmail.com>
2021-03-02 14:51:10 -1200

    the client load handler function was added to the fetch.js file

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -16,3 +16,32 @@ const insert = document.querySelector("#insert")
 const ins = document.querySelector(".ins")
 const display = document.querySelector('#display')
 const fetchCard = document.querySelector('.fetchCard');
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '269517427467-vnendark26qhr9dpqrkl7mg6nf0106k1.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyA3Xj5L-GtumYAPwsdEA8uscqpi4Gvh_Qw';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+// var inser = document.getElementById('insert');
+// var del = document.getElementById('delete');
+// var update = document.getElementById('update');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
