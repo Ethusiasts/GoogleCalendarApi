09ae97d53a6d08d71e77dd7c906efb397f050f03
Mekides <se.mekides.bishaw@gmail.com>
2021-03-02 14:46:27 -1200

    Creating the fetch.js file and variables were declared.

---
+++ assets/js/fetch.js
@@ -0,0 +1,18 @@
+// defining the variable
+let eventIdStore = new Array();
+const summery = document.querySelector("#summery")
+const calendarHomePage = document.querySelector(".home-calendar-page")
+const loc = document.querySelector("#location")
+const startDate = document.querySelector("#startDate")
+const startTime = document.querySelector("#startTime")
+const endDate = document.querySelector("#endDate")
+const endTime = document.querySelector("#endTime")
+const attendees = document.querySelector("#attendees")
+const message = document.querySelector("#message")
+
+const add =document.querySelector("#add");
+const update_eve = document.querySelector("#update-event");
+const insert = document.querySelector("#insert")
+const ins = document.querySelector(".ins")
+const display = document.querySelector('#display')
+const fetchCard = document.querySelector('.fetchCard');
