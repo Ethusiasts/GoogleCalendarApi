bb44e6f7c9d70589f429814eba0e088bdac45f38
Biruk Ayalew <se.biruk.ayalew@gmail.com>
2021-03-04 10:38:25 +0300

    a toast function is created to display error or success

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -362,3 +362,25 @@ function clearFields(){
     document.querySelector("#message").value=""
 
 }
+
+
+// toast function
+const  Toast = {
+    init(){
+        this.hideTimeout = null;
+        this.el = document.createElement('div')
+        this.el.className = 'toast';
+        document.body.appendChild(this.el)
+    },
+    show(message,state){
+        clearTimeout(this.hideTimeout);
+        this.el.textContent = message;
+        this.el.className = 'toast toast--visible';
+        if(state){
+            this.el.classList.add(`toast--${state}`);
+        }
+        this.hideTimeout = setTimeout(() => {
+            this.el.classList.remove('toast--visible');
+        },1500)
+    }
+};
